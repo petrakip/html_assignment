9fed1ebc3ba27b8b0c32d82d05f47cd57ab82653
petrakip <petraki.pg@gmail.com>
2024-12-08 18:39:27 +0200

    Exercise2 completed

---
+++ Exercise1/script.js
@@ -0,0 +1,27 @@
+// This code is about menu for responsive behaviour!
+
+let menuToggle = document.querySelector('.menuToggle');
+let header = document.querySelector('header');
+menuToggle.onclick = function () {
+    header.classList.toggle('active');
+}
+
+// Get the button
+let mybutton = document.getElementById("top-Btn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
